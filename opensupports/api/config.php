b9cb2aa4c290cf8a159538ccb6fe867d557a6111
litejamo111 <litejamo2@hotmail.com>
2024-04-03 16:49:31 -0500

    OpenSupport

---
+++ opensupports/api/config.php
@@ -0,0 +1,6 @@
+<?php
+define('MYSQL_HOST', '10.10.10.136');
+define('MYSQL_PORT', '3306');
+define('MYSQL_USER', 'ds');
+define('MYSQL_PASSWORD', 'uarJkDNmpe~b2EN');
+define('MYSQL_DATABASE', 'opensupports_130');
